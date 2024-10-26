e0705b7a0abcfc25bc6244f4a2ce2438097c63f7
nidhi-005 <cs23bt060@iitdh.ac.in>
2024-10-26 08:22:14 +0530

    Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import StudentSignup from './components/StudentSignupDEL';
+
 import Home from './components/Home';
-import Header from './components/Header';
-import StudentLogin from './components/StudentLogin';
-import Modal from './components/Modal';
-import SearchResults from './components/SearchResults';
-import BookDetails from './components/BookDetails'; // Import BookDetails component
-import MyBooks from './components/MyBooks';
-import MyHistory from './components/MyHistory'; // Import MyHistory component
+import Header from './components/Header'; // Import MyHistory component
 
 function App() {
   return (
@@ -26,10 +20,6 @@ function App() {
             <StudentSignup />
           </Modal>
         } /> */}
-        <Route path="/search-results" element={<SearchResults />} />
-        <Route path="/book-details/:id" element={<BookDetails />} /> {/* New route for BookDetails */}
-        <Route path="/my-books" element={<MyBooks />} />
-        <Route path="/my-history" element={<MyHistory />} /> {/* New route for MyHistory */}
       </Routes>
     </BrowserRouter>
   );
