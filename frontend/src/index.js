1b8fe485d3a9db35be201414883a437572ce86ba
nidhi-005 <cs23bt060@iitdh.ac.in>
2024-10-29 14:38:49 +0530

    updated googleOauth clientID

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,7 @@ const clientId = 'YOUR_GOOGLE_CLIENT_ID'; // Replace with your actual client ID
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="914973085387-m4dv9f5nbqn3skeuukr3bqnh0g61034e.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="1045088483338-1duu0l586duimuj6qup7q9r1ljn2ovlm.apps.googleusercontent.com">
       <AuthProvider>
         <App />
       </AuthProvider>
